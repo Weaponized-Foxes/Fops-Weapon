f054c3a09bb096fbf63b8b8af0bb8376bc5ce32c
Dragon Warior <evelyn.gravett@protonmail.com>
2025-01-26 01:03:29 +0000

    change how fox texture is changed

--- fox.gd
+++ fox.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
+enum FoxType {Default, Arctic}
+
 @export var speed = 400
-@export var sprite: AnimatedSprite2D
-@export var arctic = false
+@export var type = FoxType.Default
 
 func _ready() -> void:
-	if arctic:
-		sprite.sprite_frames = preload("res://ArcticFoxFrames.tres")
-	else:
-		sprite.sprite_frames = preload("res://FoxFrames.tres")
+	if type == FoxType.Arctic:
+		update_texture(preload("res://Arctic Fox Sprite Sheet.png"))
 
 func _process(delta: float) -> void:
 	pass	
@@ -18,22 +17,41 @@ func get_input() -> void:
 	velocity = direction * speed
 
 	if direction.length_squared() > 0:
-		sprite.play("run")
+		$Sprite.play("run")
 	else:
-		sprite.play("idle")
+		$Sprite.play("idle")
 	
 	if direction.x > 0:
-		sprite.flip_h = false
+		$Sprite.flip_h = false
 	elif direction.x < 0:
-		sprite.flip_h = true
+		$Sprite.flip_h = true
 
 	if Input.is_action_just_pressed("ability_1"):
-		arctic = !arctic;
-		if arctic:
-			sprite.sprite_frames = preload("res://ArcticFoxFrames.tres")
-		else:
-			sprite.sprite_frames = preload("res://FoxFrames.tres")
+		if type == FoxType.Default:
+			type = FoxType.Arctic
+			update_texture(preload("res://Arctic Fox Sprite Sheet.png"))
+		elif type == FoxType.Arctic:
+			type = FoxType.Default
+			update_texture(preload("res://Fox Sprite Sheet.png"))
 
+func update_texture(texture: Texture):
+	var reference_frames: SpriteFrames = $Sprite.sprite_frames
+	var updated_frames = SpriteFrames.new()
+
+	for animation in reference_frames.get_animation_names():
+		if animation != "default":
+			updated_frames.add_animation(animation)
+			updated_frames.set_animation_speed(animation, reference_frames.get_animation_speed(animation))
+			updated_frames.set_animation_loop(animation, reference_frames.get_animation_loop(animation))
+
+		for i in reference_frames.get_frame_count(animation):
+			var updated_texture: AtlasTexture = reference_frames.get_frame_texture(animation, i).duplicate()
+			updated_texture.atlas = texture
+			updated_frames.add_frame(animation, updated_texture)
+
+	updated_frames.remove_animation("default")
+
+	$Sprite.sprite_frames = updated_frames
 
 func _physics_process(delta: float) -> void:
 	get_input()
