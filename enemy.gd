d21721c5db0df2def8d55c536c0bd613cc1abb6d
Dragon Warior <evelyn.gravett@protonmail.com>
2025-01-28 00:35:54 +0000

    enemy health bar

--- enemy.gd
+++ enemy.gd
@@ -2,8 +2,13 @@ extends CharacterBody2D
 
 @export var health = 40
 
+func _ready() -> void:
+	$HealthBar.max_value = health
+	$HealthBar.value = health
+
 func damage(dmg: int):
 	health -= dmg
+	$HealthBar.value = health
 	if health <= 0:
 		die()
 
