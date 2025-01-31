4783082eb617ccf62785ec49d4dfa58d5293652c
Dragon Warior <evelyn.gravett@protonmail.com>
2025-01-31 12:50:59 +0000

    almost something

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,5 +1,6 @@
 extends TileMapLayer
 var rng = RandomNumberGenerator.new()
+var pathPositions: Dictionary
 
 func getPathPattern(Type: String):
 	match Type:
@@ -43,70 +44,75 @@ func getPathOptions(move: String):
 		"down":
 			return ["up-right","up-left","up-down"]
 
-func genPattern(CenterX,CenterY,Pattern: Array) -> void:
-	set_pattern(Vector2i(CenterX,CenterY),Pattern[0])
-	get_child(0).set_pattern(Vector2i(CenterX,CenterY),Pattern[1])
-	
-func HorizontalRight(pattern: TileMapPattern):
-	var size = pattern.get_size()
-	var cells = pattern.get_used_cells()
-	var shift = 0
-	for i in range(0,size.x):
-		if pattern.get_cell_atlas_coords(Vector2i(14,i)) == Vector2i(7,0) or pattern.get_cell_atlas_coords(Vector2i(14,i)) == Vector2i(7,2):
-			print(i)
-			shift += i
-			break
-		if pattern.get_cell_atlas_coords(Vector2i(0,i)) ==  Vector2i(7,0) or pattern.get_cell_atlas_coords(Vector2i(0,i)) == Vector2i(7,2):
-			print(i)
-			shift += i
-			break
-	return shift
+func genPattern(pos,pattern: int, comeFrom: String, depth: int) -> void:
+	if depth > 5:
+		return
+	set_pattern(pos,tile_set.get_pattern(pattern))
+	get_child(0).set_pattern(pos,tile_set.get_pattern(pattern + 1))
+	for direction in pathPositions[pattern].keys():
+		if direction == comeFrom:
+			continue
+		var nextDir = nextDirection(direction)
+		var patterns = []
+		for pat in pathPositions.keys():
+			if pathPositions[pat].has(nextDir):
+				patterns.append(pat)
+		if patterns.size() == 0:
+			continue
+		var chosenPattern = patterns[rng.randi_range(0, patterns.size() - 1)]
+		genPattern(calcPos(pattern, chosenPattern, direction, pos), chosenPattern, nextDir, depth + 1)
+
+
+func calcPos(pattern1, pattern2, dir_from, pos) -> Vector2i:
+	match dir_from:
+		"left":
+			pos.x -= tile_set.get_pattern(pattern2).get_size().x
+			pos.y += pathPositions[pattern1][dir_from].y - pathPositions[pattern2][nextDirection(dir_from)].y
+		"right":
+			pos.x += tile_set.get_pattern(pattern1).get_size().x
+			pos.y += pathPositions[pattern1][dir_from].y - pathPositions[pattern2][nextDirection(dir_from)].y
+		"up":
+			pos.y += tile_set.get_pattern(pattern2).get_size().y
+			pos.x += pathPositions[pattern1][dir_from].x - pathPositions[pattern2][nextDirection(dir_from)].x
+		"down":
+			pos.y -= tile_set.get_pattern(pattern2).get_size().y
+			pos.x += pathPositions[pattern1][dir_from].x - pathPositions[pattern2][nextDirection(dir_from)].x
+	return pos
+		
 
 func pathGen():
-	var Lx = -8
-	var Ly = -8
-	var Rx = -8
-	var Ry = -8
-	var Lmove = "left"
-	var Rmove = "right"
-	var Lpattern = getPathPattern("left-right")
-	genPattern(Lx,Ly,Lpattern)
-	
-	var choice = getPathOptions(Lmove).pick_random()
-	Lpattern = getPathPattern(choice)
+	genPattern(Vector2i(0,-7), 0, "", 0)
 
+func nextDirection(direction: String):
+	match direction:
+		"right":
+			return "left"
+		"left":
+			return "right"
+		"up":
+			return "down"
+		"down":
+			return "up"
+		
 
+func setupGenerator():
+	var tiles = [0,2,4,6,8,10]
+	pathPositions = {}
+	for tile in tiles:
+		pathPositions[tile] = {}
+		var pattern = tile_set.get_pattern(tile)
+		var size = pattern.get_size()
+		for i in range(size.y):
+			if pattern.get_cell_atlas_coords(Vector2i(0, i)) == Vector2i(7,0) || pattern.get_cell_atlas_coords(Vector2i(0, i)) == Vector2i(7,2):
+				pathPositions[tile]["left"] = Vector2i(0,i)
+			if pattern.get_cell_atlas_coords(Vector2i(size.x - 1, i)) == Vector2i(7,0) || pattern.get_cell_atlas_coords(Vector2i(size.x - 1, i)) == Vector2i(7,2):
+				pathPositions[tile]["right"] = Vector2i(size.x - 1,i)
+		for i in range(size.x):
+			if pattern.get_cell_atlas_coords(Vector2i(i, 0)) == Vector2i(6,1) || pattern.get_cell_atlas_coords(Vector2i(i, 0)) == Vector2i(8,1):
+				pathPositions[tile]["down"] = Vector2i(i,0)
+			if pattern.get_cell_atlas_coords(Vector2i(i, size.y - 1)) == Vector2i(6, 1) || pattern.get_cell_atlas_coords(Vector2i(i, size.y - 1)) == Vector2i(8, 1):
+				pathPositions[tile]["up"] = Vector2i(i,size.y - 1)
 
-
-
-
-
-	
-
-
-
-	
-
-
-
-	
-	
-	
-
-	
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#randomGen()
+	setupGenerator()
 	pathGen()
-
-	
-	pass # Replace with function body.
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	pass
